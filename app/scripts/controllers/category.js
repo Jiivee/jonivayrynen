d3d26271fe0fd5920e95b7c7f268f6891cb7d523
Jiivee <joni.vayrynen@gmail.com>
2015-08-08 12:44:39 +0300

    cache test

--- app/scripts/controllers/category.js
+++ app/scripts/controllers/category.js
@@ -3,7 +3,7 @@
 angular.module('jonivayrynenApp')
   .controller('CategoryCtrl', function ($scope, $http, $routeParams) {
     $scope.categoryname = $routeParams.categoryname;
-    $http.get('artwork/artwork.json').success(function(data) {
+    $http.get('artwork/artwork.json', { headers: { 'Cache-Control' : 'no-cache' } } ).success(function(data) {
       $scope.artworks = data;
       $scope.category = data[$scope.categoryname];
     });
